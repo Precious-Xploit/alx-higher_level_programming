f82139c716be816a13f4a94a429bcd14861bdf7f
Precious-Xploit <preciouxxploit@gmail.com>
2022-12-03 14:02:49 +0000

    Data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    return my_string.translate({ord(i): None for i in 'cC'})
+    new_string = []
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        new_string.append(i)
+    return ("".join(new_string))
