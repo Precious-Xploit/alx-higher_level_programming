60a9b9ec1b30ac4c826f3f1e639fd7debf619d4b
Precious-Xploit <preciouxxploit@gmail.com>
2023-01-10 08:47:02 +0000

    Python

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Defines a class MyList that inherits from list"""
+"""
+This program creates a class MyList that inherits of the class List
+"""
 
 
 class MyList(list):
-    """Class that inherits from list.
-    Args:
-        list (list): list to sort in ascending order.
     """
+    This class inherits form the class list an can print it's elements sorted
+    """
+
     def print_sorted(self):
-        """Prints a list in ascending order.
-        """
-        list_ = self[:]
-        list_.sort()
-        print(list_)
+        print(sorted(self))	
